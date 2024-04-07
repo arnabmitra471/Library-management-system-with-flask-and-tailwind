14c0c6aa9ed7c3c34b75c730679b3cecbf4e6b80
Arnab Mitra <arnabmitra471@gmail.com>
2024-04-07 08:47:06 +0530

    Connected mysql database with the flask app and storing user data

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask,render_template,request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MySQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "user_reg_details"
+
+db = MySQL(app)
+
+class User:
+    def __init__(self,name,email,password,cPassword):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.cPassword = cPassword
+
 
 @app.route("/")
 def home_page():
@@ -13,10 +28,14 @@ def login_page():
 @app.route("/signup",methods=["GET","POST"])
 def signup_page():
     if request.method == "POST":
-        name = request.form.get("name")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        confirm_pass = request.form.get("cPassword")
+        name = request.form.get("name",type=str)
+        email = request.form.get("email",type=str)
+        password = request.form.get("password",type=str)
+        confirm_pass = request.form.get("cPassword",type=str)
+
+        cursor = db.connection.cursor()
+        cursor.execute("INSERT INTO user_registration_data (name,email,password,confirm_pass) values(%s, %s, %s, %s)",(name,email,password,confirm_pass))
+        db.connection.commit()
     return render_template("signup.html")
 
 # endpoint to fetch and display all books
